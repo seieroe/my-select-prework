56c64b0634105c32c98fda767ab6b4cfc8ef478a
emily seieroe <e.e.seieroe@gmail.com>
2018-12-09 06:12:30 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
+  array_new = []
+  i = 0 
+  while i < collection.length 
+    value = collection[i]
+    if yield value 
+      array_new << value 
+    end
+    
+    i += 1
+  end
+  array_new
  # your code here!
 end
